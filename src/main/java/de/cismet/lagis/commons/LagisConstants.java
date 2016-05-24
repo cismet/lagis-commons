4a51f06c45debc5f07ffae97ac6fd2ad4664f70c
JeanAtCismet <jean.ruiz@cismet.de>
2016-05-24 12:01:00 +0200

    fixed #1 introduction of metaclass constants

---
+++ src/main/java/de/cismet/lagis/commons/LagisConstants.java
@@ -0,0 +1,26 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.lagis.commons;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public class LagisConstants {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String DOMAIN_LAGIS = "LAGIS";
+}
